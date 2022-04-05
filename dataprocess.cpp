d20fe49fe103e74a86263d7a03d98db7dda55ba1
dwvwdv <dwvwdv@protonmail.com>
2022-04-05 17:51:07 +0800

    檔案處理完成，現在可基本瀏覽修改Code

---
+++ dataprocess.cpp
@@ -0,0 +1,6 @@
+#include "dataprocess.h"
+
+dataprocess::dataprocess()
+{
+
+}
---
+++ dataprocess.h
@@ -0,0 +1,11 @@
+#ifndef DATAPROCESS_H
+#define DATAPROCESS_H
+
+
+class dataprocess
+{
+public:
+    dataprocess();
+};
+
+#endif // DATAPROCESS_H
